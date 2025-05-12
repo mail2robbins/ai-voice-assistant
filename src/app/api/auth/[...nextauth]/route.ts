8dd21fc14a154902c62bf4dee93588e94fa691b2
mail2robbins <mail2robbins@gmail.com>
2025-05-11 23:40:20 -0400

    fixed a build error in auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -58,6 +58,7 @@ const handler = NextAuth({
   },
   session: {
     strategy: 'jwt',
+    maxAge: 30 * 24 * 60 * 60, // 30 days
   },
   secret: process.env.NEXTAUTH_SECRET,
   debug: false, // Enable debug logs
